314234b1a1a1fb98f6a9d8c92a1e89fc8be9dcdf
Akihiro Kondo <akihyro@gmail.com>
2016-02-26 23:32:25 +0900

    Add Retryable class

---
+++ src/main/java/net/rakugakibox/retryable/core/Retryable.java
@@ -0,0 +1,102 @@
+package net.rakugakibox.retryable.core;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import static java.util.Arrays.asList;
+import java.util.Collection;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.unmodifiableList;
+import lombok.NonNull;
+
+/**
+ * Retryable main class.
+ */
+public class Retryable {
+
+    /**
+     * Number of tries.
+     */
+    long tries = 2;
+
+    /**
+     * Retrying exception classes.
+     */
+    Collection<Class<? extends Throwable>> exceptions = singletonList(Throwable.class);
+
+    /**
+     * Retrying interval..
+     */
+    Duration interval = Duration.ZERO;
+
+    /**
+     * Set number of tries.
+     * Should an exception occur, it'll retry for {@code (tries - 1)} times.
+     *
+     * @param tries number of tries.
+     * @return this instance.
+     */
+    public Retryable tries(long tries) {
+        this.tries = tries;
+        return this;
+    }
+
+    /**
+     * Set retrying exception classes.
+     *
+     * @param exceptions retrying exception classes.
+     * @return this instance.
+     */
+    @SafeVarargs
+    @SuppressWarnings("varargs")
+    public final Retryable on(@NonNull Class<? extends Throwable>... exceptions) {
+        return on(asList(exceptions));
+    }
+
+    /**
+     * Set retrying exception classes.
+     *
+     * @param exceptions retrying exception classes.
+     * @return this instance.
+     */
+    public Retryable on(@NonNull Collection<Class<? extends Throwable>> exceptions) {
+        this.exceptions = unmodifiableList(new ArrayList<>(exceptions));
+        return this;
+    }
+
+    /**
+     * Set retrying interval.
+     *
+     * @param interval retrying interval.
+     * @return this instance.
+     */
+    public Retryable interval(@NonNull Duration interval) {
+        this.interval = interval;
+        return this;
+    }
+
+    /**
+     * Run the specified function.
+     * If an particular exception occurs, retries.
+     *
+     * @param <T> result type of the function.
+     * @param function retryable function.
+     * @return result of the function.
+     */
+    public <T> T run(@NonNull RetryableFunction<T> function) {
+        RetryableContext context = new RetryableContext();
+        while (context.times < tries) {
+            try {
+                context.times++;
+                return function.apply(context);
+            } catch (Throwable exc) {
+                context.exceptions.add(exc);
+                if (!exceptions.stream().anyMatch(c -> c.isInstance(exc))) {
+                    throw new RuntimeException(exc);  // TODO: Add original exception class.
+                }
+            }
+            // TODO: Thread.sleep(interval.toMillis(), interval.getNano());
+        }
+        throw new RuntimeException();  // TODO: Add original exception class.
+    }
+
+}
--- src/main/java/net/rakugakibox/retryable/core/RetryableContext.java
+++ src/main/java/net/rakugakibox/retryable/core/RetryableContext.java
@@ -1,7 +1,40 @@
 package net.rakugakibox.retryable.core;
 
+import java.util.ArrayList;
+import static java.util.Collections.unmodifiableList;
+import java.util.List;
+
 /**
  * Retryable context.
  */
 public class RetryableContext {
+
+    /**
+     * Number of times.
+     */
+    long times = 0;
+
+    /**
+     * Exceptions that occurred in function.
+     */
+    List<Throwable> exceptions = new ArrayList<>();
+
+    /**
+     * Return number of times.
+     *
+     * @return number of times.
+     */
+    public long times() {
+        return times;
+    }
+
+    /**
+     * Return exceptions that occurred in function.
+     *
+     * @return exceptions that occurred in function.
+     */
+    public List<Throwable> exceptions() {
+        return unmodifiableList(exceptions);
+    }
+
 }
