ea1f778c5d3dfc7e79cb659fe742a16533284b59
Akihiro Kondo <akihyro@gmail.com>
2016-02-26 22:07:46 +0900

    RetryableFunction: Modify throws type.

--- src/main/java/net/rakugakibox/retryable/core/RetryableFunction.java
+++ src/main/java/net/rakugakibox/retryable/core/RetryableFunction.java
@@ -4,18 +4,17 @@ package net.rakugakibox.retryable.core;
  * Retryable function.
  *
  * @param <T> result type of the function.
- * @param <E> exception type of the function.
  */
 @FunctionalInterface
-public interface RetryableFunction<T, E extends Throwable> {
+public interface RetryableFunction<T> {
 
     /**
      * Apply the function.
      *
      * @param context context of the function.
      * @return result of the function.
-     * @throws E if an exception occurs in the function.
+     * @throws Throwable if an exception occurs in the function.
      */
-    T apply(RetryableContext context) throws E;
+    T apply(RetryableContext context) throws Throwable;
 
 }
