0eb565b8fe45ebabff9875d4b8dbec2e7cba71f6
Akihiro Kondo <akihyro@gmail.com>
2016-04-30 15:36:49 +0900

    Improve package info

--- src/main/java/net/rakugakibox/retryable/package-info.java
+++ src/main/java/net/rakugakibox/retryable/package-info.java
@@ -1,5 +1,5 @@
 /**
- * The module provides a retry of the code block.
+ * The module provides a retry of the code block for Java 8+.
  * Runs a code block, and retries it when an exception occurs.
  */
 package net.rakugakibox.retryable;
